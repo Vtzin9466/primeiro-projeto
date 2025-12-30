61530a55651b5d9bcb9000440b2d537813fa548d
Vtzin9466 <pagottovitor06@gmail.com>
2025-12-30 18:15:27 -0300

    começando

--- registro_de_vendas.py
+++ registro_de_vendas.py
@@ -1,1 +1,10 @@
-class RegistroDeVendas:
+import requests
+url = 'https://app.lojaintegrada.com.br/painel/plataforma/vender'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    dados = response.json()
+    print(dados)
+else:
+    print("Erro ao acessar API:", response.status_code)
